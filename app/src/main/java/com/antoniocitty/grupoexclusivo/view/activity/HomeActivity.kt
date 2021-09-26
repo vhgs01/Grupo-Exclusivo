398ac5012de1c6a1a09b433075ccfe02382e7ee2
Victor Hugo <victor_hgsilva@hotmail.com>
2021-09-26 18:38:29 -0300

    Adicionando FAB para suporte no whatsapp

--- app/src/main/java/com/antoniocitty/grupoexclusivo/view/activity/HomeActivity.kt
+++ app/src/main/java/com/antoniocitty/grupoexclusivo/view/activity/HomeActivity.kt
@@ -2,10 +2,11 @@ package com.antoniocitty.grupoexclusivo.view.activity
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.antoniocitty.grupoexclusivo.R
 import com.antoniocitty.grupoexclusivo.contract.HomeContract
 import com.antoniocitty.grupoexclusivo.presenter.HomePresenter
@@ -15,6 +16,8 @@ import com.antoniocitty.grupoexclusivo.util.SharedPreferences.SHARED_PREFERENCES
 import kotlinx.android.synthetic.main.activity_home.*
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
+import java.net.URLEncoder
+
 
 class HomeActivity : AppCompatActivity(), HomeContract.View {
 
@@ -22,6 +25,9 @@ class HomeActivity : AppCompatActivity(), HomeContract.View {
         private const val WEBVIEW_TO_OPEN = "webview_to_open"
         private const val HANDOUT = "handout"
         private const val PURCHASE = "purchase"
+        private const val WHATS_PHONE = "5528999511880"
+        private const val WHATS_MESSAGE =
+            "Olá Antônio! Estava usando o aplicativo e fiquei com uma dúvida, poderia me ajudar?"
     }
 
     private val presenter: HomePresenter by inject { parametersOf(this) }
@@ -38,7 +44,8 @@ class HomeActivity : AppCompatActivity(), HomeContract.View {
         val prefs = getSharedPreferences(SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE)
         val courseIsCompleted = prefs.getBoolean(FIELD_COURSE_COMPLETED, false)
 
-        if (courseIsCompleted) cardPurchase.visibility = View.VISIBLE else cardPurchase.visibility = View.GONE
+        if (courseIsCompleted) cardPurchase.visibility = View.VISIBLE else cardPurchase.visibility =
+            View.GONE
     }
 
     override fun setListeners() {
@@ -46,6 +53,7 @@ class HomeActivity : AppCompatActivity(), HomeContract.View {
         setLogoutButtonListener()
         setHandoutButtonListener()
         setPurchaseButtonListener()
+        setFabListener()
     }
 
     override fun handleAnimation(startAnimation: Boolean) {
@@ -98,6 +106,26 @@ class HomeActivity : AppCompatActivity(), HomeContract.View {
 
             intent.putExtra(WEBVIEW_TO_OPEN, PURCHASE)
             startActivity(intent)
+        }
+    }
+
+    private fun setFabListener() {
+        favWhats.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+
+            try {
+                val url =
+                    "https://api.whatsapp.com/send?phone=$WHATS_PHONE&text=" + URLEncoder.encode(
+                        WHATS_MESSAGE,
+                        "UTF-8"
+                    )
+
+                intent.setPackage("com.whatsapp")
+                intent.data = Uri.parse(url)
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
